cec2d50cbf4f8ba090394071f40bddd8f094c93e
ManasMish <manasmishra845@gmail.com>
2024-03-03 00:17:40 +0530

    3rd March 2024 00:17

---
+++ src/test/java/com/tta/base/BaseTest.java
@@ -0,0 +1,33 @@
+package com.tta.base;
+
+import com.tta.actions.AssertActions;
+import com.tta.endpoints.APIConstants;
+import com.tta.modules.PayloadManager;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+
+    public RequestSpecification requestSpecification;
+    public AssertActions assertActions;
+    public PayloadManager payloadManager;
+    public JsonPath jsonPath;
+    public Response response;
+    public ValidatableResponse validatableResponse;
+
+
+    @BeforeTest
+    public void setConfig(){
+        payloadManager = new PayloadManager();
+        assertActions = new AssertActions();
+        requestSpecification = new RequestSpecBuilder()
+                .setBaseUri(APIConstants.BASE_URL)
+                .addHeader("Content-Type","application/json")
+                .build().log().all();
+    }
+}
---
+++ src/test/java/com/tta/tests/e2e/TC1.java
@@ -0,0 +1,4 @@
+package com.tta.tests.e2e;
+
+public class TC1 {
+}
